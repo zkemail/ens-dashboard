4e80a1ffe964e47dccba81fb644a80adb3ca0da6
zkfriendly <zkfriendly@proton.me>
2025-09-18 16:23:11 +0200

    fix: set hmr to false

--- server.ts
+++ server.ts
@@ -8,12 +8,14 @@ Bun.serve({
     const url = new URL(req.url);
     if (url.pathname === "/x.eml") {
       const file = await Bun.file("./x.eml").text();
-      return new Response(file, { headers: { "content-type": "message/rfc822; charset=utf-8" } });
+      return new Response(file, {
+        headers: { "content-type": "message/rfc822; charset=utf-8" },
+      });
     }
     return new Response("Not Found", { status: 404 });
   },
   development: {
-    hmr: true,
+    hmr: false,
     console: true,
   },
 });
