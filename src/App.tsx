73a0de11b6eec9066264f7347b3802bf17f4c1cd
zkfriendly <zkfriendly@proton.me>
2025-09-29 11:07:31 +0200

    fix: move web3 provider to root

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import "./App.css";
-import { Web3Provider } from "./components/Web3Provider";
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { ProfilePage } from "./pages/ProfilePage";
 
 function App() {
   return (
-    <Web3Provider>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/name/:ensName" element={<ProfilePage />} />
-      </Routes>
-    </Web3Provider>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/name/:ensName" element={<ProfilePage />} />
+    </Routes>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { Web3Provider } from "./components/Web3Provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Web3Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Web3Provider>
   </StrictMode>
 );
