fabcc29ad583a1f849bf8d2a1972a7f3300f4fab
Bence Háromi <56651250+benceharomi@users.noreply.github.com>
2026-02-18 19:13:42 +0000

    feat: google sign-in

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { ProfilePage } from "./pages/ProfilePage";
 import { BenchmarkPage } from "./pages/BenchmarkPage";
+import { AuthCallbackPage } from "./pages/AuthCallbackPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/name/:ensName" element={<ProfilePage />} />
       <Route path="/benchmark" element={<BenchmarkPage />} />
+      <Route path="/auth/callback" element={<AuthCallbackPage />} />
     </Routes>
   );
 }
